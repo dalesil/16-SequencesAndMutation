45dc2e2c8ae01ebca53b025f4c84a4f216e58fcf
Lucas D'Alesio <dalesil@rose-hulman.edu>
2019-01-23 00:58:49 -0500

    m7 done

--- src/m2_more_sequences.py
+++ src/m2_more_sequences.py
@@ -103,10 +103,15 @@ def shortest_string(strings):
       :type strings: list[str]   or tuple(str)
     """
     # ------------------------------------------------------------------
-    # TODO: 2. Implement and test this function.
+    # DONE: 2. Implement and test this function.
     #     The testing code is already written for you (above).
     # ------------------------------------------------------------------
 
+    short = strings[0]
+    for k in range(len(strings)):
+        if len(strings[k]) < len(short):
+            short = strings[k]
+    return short
 
 
 def run_test_index_of_largest_number():
@@ -181,10 +186,15 @@ def index_of_largest_number(numbers, n):
 
     """
     # ------------------------------------------------------------------
-    # TODO: 3. Implement and test this function.
+    # DONE: 3. Implement and test this function.
     #     The testing code is already written for you (above).
     # ------------------------------------------------------------------
 
+    x = 0
+    for k in range(n):
+        if numbers[k] > numbers[x]:
+            x = k
+    return x
 
 # ----------------------------------------------------------------------
 # Some problems iterate (loop) through the sequence accessing TWO
@@ -236,10 +246,15 @@ def number_of_stutters(s):
        :type s: str
     """
     # ------------------------------------------------------------------
-    # TODO: 4. Implement and test this function.
+    # DONE: 4. Implement and test this function.
     #     The testing code is already written for you (above).
     # ------------------------------------------------------------------
 
+    total = 0
+    for k in range(len(s)-1):
+        if s[k+1] == s[k]:
+            total = total + 1
+    return total
 
 def run_test_is_palindrome():
     """ Tests the   is_palindrome   function. """
@@ -309,7 +324,7 @@ def is_palindrome(s):
       :type s: str
     """
     # ------------------------------------------------------------------
-    # TODO: 5. Implement and test this function.
+    # DONE: 5. Implement and test this function.
     #     The testing code is already written for you (above).
     #
     ####################################################################
@@ -319,6 +334,10 @@ def is_palindrome(s):
     ####################################################################
     # ------------------------------------------------------------------
 
+    for k in range(len(s)//2):
+        if s[k] != s[len(s)-1-k]:
+            return False
+    return True
 
 # ----------------------------------------------------------------------
 # Some problems loop (iterate) through two or more sequences
@@ -376,10 +395,15 @@ def count_same(sequence1, sequence2):
       type: sequence2: tuple or list or string
     """
     # ------------------------------------------------------------------
-    # TODO: 6. Implement and test this function.
+    # DONE: 6. Implement and test this function.
     #     The testing code is already written for you (above).
     # ------------------------------------------------------------------
 
+    x = 0
+    for k in range(len(sequence2)):
+        if sequence2[k] == sequence1[k]:
+            x = x + 1
+    return x
 
 # ----------------------------------------------------------------------
 # Calls  main  to start the ball rolling.
